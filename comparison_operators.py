2bac0823886eff858e6b932ce8f3b6472211106d
SHIVANAND <shivanandmishra14@gmail.com>
2022-02-23 09:26:17 +0530

    operators etc

---
+++ comparison_operators.py
@@ -0,0 +1,13 @@
+from sys import base_prefix
+
+
+a = (11>10)
+b = (20<30)
+c = (69<69)
+print(a,b,c)
+
+i = (10 ==10)
+print(i)
+
+j = (21 != 20)
+print(j)
---
+++ logical_operators.py
@@ -0,0 +1,16 @@
+# or, not, and and so on
+
+bool1 = True
+bool2 = False
+
+print("Values are", (bool1 and bool2))
+print("Values are", (bool1 or bool2))
+
+# revers the values with not oiperators 
+print("Values are", ( not bool2))  
+
+
+# revers the values with not oiperators
+print("Values are", (not bool1))
+
+
