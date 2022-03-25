bfd5efaa04fd475a2378726fbf8e431e29e0595d
SHIVANAND <shivanandmishra14@gmail.com>
2022-03-25 09:15:13 +0530

    lists

---
+++ list.py
@@ -0,0 +1,12 @@
+a = [1,2,4,5,76]
+print(a)
+print(a[0])
+print(a[3])
+
+# Add new value
+a[0]= 32
+print(a)
+
+jumble = ["a", "bo", "ce", "we", 44]
+print(jumble[0:4])
+print(jumble[-4:])
---
+++ list_methods.py
@@ -0,0 +1,32 @@
+# sort : means increasing order
+l1 = [1,4,7,6,8,4]
+# l_sorted = l1.sort()
+# print(l_sorted)
+l1.sort()
+print(l1)
+
+# reverse
+l2 = [1,4,7,6,8,4]
+l2.reverse()
+print(l2)
+
+# append : means add anything
+l3 = [1,4,7,6,8,4]
+l3.append(45)
+l3.append(65)
+print(l3)
+
+# Insert
+l4 = [1,4,7,6,8,4]
+l4.insert(1, 72)
+print(l4)
+
+# pop
+l5 = [1,4,7,6,8,4]
+l5.pop(5)
+print(l5)
+
+# remove
+l6 = [1,4,7,6,8,4]
+l6.remove(8)
+print(l6)
